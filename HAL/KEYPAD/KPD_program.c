a2b61f019ee27aaef37803569beffc74d93b31e7
“Kerolos <kerolos.nashaat.kn@gmail.com>
2022-07-05 00:30:51 +0200

    Finalizing the driver

--- HAL/KEYPAD/KPD_program.c
+++ HAL/KEYPAD/KPD_program.c
@@ -22,22 +22,33 @@
 u8 KPD_u8PressedKey(){
 	u8 Local_u8PressedKey = KPD_NO_KEY_IS_PRESSED;
 	u8 Local_u8RowIterator, Local_u8ColumnIterator, Local_u8Value = 1;
-	u8 Local_u8Array[KPD_ROWS][KPD_COLUMNS]= KPD_ARRAY_VALUES;
-	u8 Local_u8ColumnArray[KPD_COLUMNS] = {KPD_COLUMN0, KPD_COLUMN1, KPD_COLUMN2, KPD_COLUMN3};
-	u8 Local_u8ROWArray[KPD_ROWS] = {KPD_ROW0, KPD_ROW1, KPD_ROW2, KPD_ROW3};
+	static u8 Local_u8Array[KPD_ROWS][KPD_COLUMNS]= KPD_ARRAY_VALUES;
+	static u8 Local_u8ColumnArray[KPD_COLUMNS] = {KPD_COLUMN0, KPD_COLUMN1, KPD_COLUMN2, KPD_COLUMN3};
+	static u8 Local_u8ROWArray[KPD_ROWS] = {KPD_ROW0, KPD_ROW1, KPD_ROW2, KPD_ROW3};
 
 	for(Local_u8ColumnIterator = 0 ; Local_u8ColumnIterator < KPD_COLUMNS ; Local_u8ColumnIterator++ ){
+		/* Current Column Activation */
 		DIO_u8SetPinValue(KPD_PORT, Local_u8ColumnArray[Local_u8ColumnIterator], DIO_u8PIN_LOW);
+		/* Scanning the Rows */
 		for(Local_u8RowIterator = 0 ; Local_u8RowIterator < KPD_ROWS ; Local_u8RowIterator++ ){
+			/* Row Reading */
 			DIO_u8GetPinValue(KPD_PORT, Local_u8ROWArray[Local_u8RowIterator], &Local_u8Value);
-			delay(50);
-			if( Local_u8Value != 1 ){
+			/* Check if a switch is pressed */
+			if( Local_u8Value == 0 ){
+				/* Assigning the value from the array */
 				Local_u8PressedKey = Local_u8Array[Local_u8RowIterator][Local_u8ColumnIterator];
-				break;
+				/* Checking for a Rising Edge */
+				while( Local_u8Value == 0 ){
+					/* Row Reading */
+					DIO_u8GetPinValue(KPD_PORT, Local_u8ROWArray[Local_u8RowIterator], &Local_u8Value);
+				}
+				/* Current Column Deactivate */
+				DIO_u8SetPinValue(KPD_PORT, Local_u8ColumnArray[Local_u8ColumnIterator], DIO_u8PIN_HIGH);
+				return Local_u8PressedKey;
 			}
 		}
+		/* Current Column Deactivate */
 		DIO_u8SetPinValue(KPD_PORT, Local_u8ColumnArray[Local_u8ColumnIterator], DIO_u8PIN_HIGH);
-		if( Local_u8Value != 1 ) break;
 	}
 	return Local_u8PressedKey;
 }
